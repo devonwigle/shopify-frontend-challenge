ecea783deeb75e8e6c3f9a8036266b5668cc2305
Devon Wigle <devonwigle@gmail.com>
2022-05-12 13:09:55 -0500

    Remove boilerplate and add imports and basic html

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import React from 'react'
+import Form from './Components/Form/Form'
+import ResponseWrapper from './Components/ResponseWrapper/ResponseWrapper'
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Fun with AI</h1>
+      <Form />
+      <ResponseWrapper />
+    </main>
   );
 }
 
